e01af1674785cf05820a6d170266d027d5b1afcf
NTrubitcyn <nikitos99099@gmail.com>
2025-10-12 08:33:01 +0300

    Fix to bb diff to unified hunk

--- src/revu/infrastructure/git_providers/bitbucket/helpers.py
+++ src/revu/infrastructure/git_providers/bitbucket/helpers.py
@@ -11,8 +11,8 @@ PREFIX = {
 def json_diff_to_unified(diff_json) -> str:
     lines = []
     for file_diff in diff_json['diffs']:
-        src = file_diff['source']['toString']
-        dst = file_diff['destination']['toString']
+        src = file_diff.get('source', {}).get('toString', None)
+        dst = file_diff.get('destination', {}).get('toString', None)
         lines.append(f"diff --git a/{src} b/{dst}")
         lines.append(f'--- a/{src}')
         lines.append(f'+++ b/{dst}')
@@ -22,10 +22,10 @@ def json_diff_to_unified(diff_json) -> str:
             src_span = hunk['sourceSpan']
             dst_start = hunk['destinationLine']
             dst_span = hunk['destinationSpan']
-            lines.append(f"@@ -{src_start},{src_span} + {dst_start},{dst_span} @@")
+            lines.append(f"@@ -{src_start},{src_span} +{dst_start},{dst_span} @@")
             
             for segment in hunk['segments']:
                 prefix = PREFIX[segment['type']]
                 for line in segment['lines']:
-                    lines.append(f"{prefix}{line['line']}")
+                    lines.append(f"{prefix}{line.get('line', '')}")
     return '\n'.join(lines)
